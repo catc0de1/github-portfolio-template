f91e4f8a0c22d0ccaf64b0c49b600088b9450f5c
Iyan Z Armanda <catcode0101@gmail.com>
2026-04-16 19:02:49 +0700

    fix: change contact name and fix error on tech stack
    - if tech stack name unknown, it will display none

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -18,7 +18,7 @@ export function projectCard(project) {
     ? project.techStack.map((t) => {
       const tech = techBrand[t.name];
       if (!tech) {
-        // return `<span class="project-tech-item unknown">${t.name}</span>`;
+        return `<span style="display: none;">Unknown ${t.name}</span>`;
       }
       return `
         <span class="project-tech-item" title="${tech.name}">
